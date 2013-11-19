f07b3d015bbb36e7fc9d5bb01c57024e2901029c
Ryan Hefner <ryan@vimeo.com>
2013-11-19 17:11:20 -0500

    Fix fileName reference.

--- BuildAntTask-SublimeText-Plugin.py
+++ BuildAntTask-SublimeText-Plugin.py
@@ -2,10 +2,10 @@ import sublime, sublime_plugin
 
 class BuildAntTasksCommand(sublime_plugin.TextCommand):
 	def onLoad(self, view):
-		print view.filename(), 'just got loaded'
+		print view.fileName(), 'just got loaded'
 
 	def onModified(self, view):
-		print view.filename(), 'just got modified'
+		print view.fileName(), 'modified'
 
 	def run(self, edit):
 		self.view.insert(edit, 0, "Hello, World!")
